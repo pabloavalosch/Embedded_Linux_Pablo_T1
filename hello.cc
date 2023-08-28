7fc09601b73c4797eef77ed228e5712df6a4437f
Pablo Avalos <ie714518@iteso.mx>
2023-08-28 15:59:44 -0600

    Uploading hello world code

---
+++ hello.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <unistd.h>
+
+int main(int argc, char * argv[] ) {
+
+        for(;;) {
+
+                std::cout << "HOla mundo" << std::endl;
+                sleep(1);
+        }
+
+        return 0;
+
+}
